21bfacc6e1c35a8a66aa01f326d138ff0d82deb5
Noxolo <Noxoloshlatshwayo@gmail.com>
2022-06-08 14:22:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,7 +21,7 @@ void print_times_table(int n)
 			{
 				_putchar(z / 100 + '0');
 				_putchar((z / 10 % 10) + '0');
-				_pitchar(z % 10 + '0');
+				_putchar(z % 10 + '0');
 			else if (z > 9)
 			{
 				_putchar(' ');
