000cb0d5b72e0af9891dd5bc7689a29540f0894c
Noxolo <Noxoloshlatshwayo@gmail.com>
2022-06-08 11:38:04 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,12 @@
  */
 void jack_bauer(void)
 {
-	int H, M;
+	int H;
 
 	for (H = 0; H < 24; H++)
 	{
+		int M;
+
 		for (M = 0; M < 60; M++)
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
