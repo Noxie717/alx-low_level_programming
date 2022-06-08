ae286717920ff60c87732ffea4571af2fd66b703
Noxolo <Noxoloshlatshwayo@gmail.com>
2022-06-08 13:13:38 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds teo integers and returns the result.
+ * @a: An integer a
+ * @b: An integer b
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
