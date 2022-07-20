8ef4814541413434e2556b85edff63193f692cd4
Noxolo <Noxoloshlatshwayo@gmail.com>
2022-07-20 03:47:00 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure type defined as dog
+ * @name: character string
+ * @age: float
+ * @owner: character string
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
